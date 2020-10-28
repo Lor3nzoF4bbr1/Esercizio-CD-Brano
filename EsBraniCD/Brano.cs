2fa6f0b28fb3541307c778dd1250ffc26dd388a2
Lorenzo Fabbri <LORENZO.FABBRI.4G@local.ittcesena.it>
2020-10-28 11:43:30 +0100

    Creazione proprietà di Brano

--- EsBraniCD/Brano.cs
+++ EsBraniCD/Brano.cs
@@ -8,5 +8,29 @@ namespace EsBraniCD
 {
     class Brano
     {
-    }
+        public Brano(string titolo, string autore, double durata)
+        {
+            Titolo = titolo;
+            Autore = autore;
+            Durata = durata;
+        }
+
+        public string Titolo
+        {
+            get;
+            set;
+        }
+
+        public string Autore
+        {
+            get;
+            set;
+        }
+
+        public double Durata
+        {
+            get;
+            set;
+        }
+    } 
 }
